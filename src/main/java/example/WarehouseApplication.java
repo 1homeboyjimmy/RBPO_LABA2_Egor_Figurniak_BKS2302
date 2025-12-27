bd98c5192899213f0f5982099fea19cf2daf88e2
1homeboyjimmy <eat0703@gmail.com>
2025-12-27 18:18:21 +0300

    feat(lab5)

--- src/main/java/example/WarehouseApplication.java
+++ src/main/java/example/WarehouseApplication.java
@@ -1,6 +1,5 @@
 package example;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,11 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WarehouseApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().load();
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-
         SpringApplication.run(WarehouseApplication.class, args);
     }
 
